2f468503c31aac6968c182f3efe0c43391705709
Anton Guryev <ratatoskr2019@gmail.com>
2022-02-08 17:55:33 +0300

    Factory Added

--- app/src/main/java/ru/ratatoskr/project_3/presentation/activity/MainActivity.kt
+++ app/src/main/java/ru/ratatoskr/project_3/presentation/activity/MainActivity.kt
@@ -1,10 +1,10 @@
 package ru.ratatoskr.project_3.presentation.activity
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -12,6 +12,7 @@ import ru.ratatoskr.project_3.data.impl.HeroesRepoImpl
 import ru.ratatoskr.project_3.domain.model.Hero
 import ru.ratatoskr.project_3.presentation.composable.MyComposable
 import ru.ratatoskr.project_3.presentation.viewmodel.MainViewModel
+import ru.ratatoskr.project_3.presentation.viewmodel.MainViewModelFactory
 
 sealed class Routes(val route: String) {
     object HeroesList : Routes("HeroesList")
@@ -21,14 +22,18 @@ sealed class Routes(val route: String) {
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel = MainViewModel(repository = HeroesRepoImpl())
-
-    //TODO ViewModels Factory :D
+    lateinit var viewModel: MainViewModel
 
     var myComposable = MyComposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(
+            this,
+            MainViewModelFactory(HeroesRepoImpl())
+        )[MainViewModel::class.java]
 
         setContent {
             myComposable.Wrapper { myComposable.WaitScreen() }
@@ -39,18 +44,18 @@ class MainActivity : AppCompatActivity() {
                 setContent {
                     val navController = rememberNavController()
                     var heroesList: List<Hero> = remember { viewModel.HeroesList.value!! }
-                    NavHost(navController, startDestination = Routes.HeroesList.route){
-                        composable(Routes.HeroesList.route){
+                    NavHost(navController, startDestination = Routes.HeroesList.route) {
+                        composable(Routes.HeroesList.route) {
                             myComposable.Wrapper {
-                                myComposable.HeroesScreen(heroesList,navController)
+                                myComposable.HeroesScreen(heroesList, navController)
                             }
                         }
-                        composable(Routes.WaitScreen.route){
+                        composable(Routes.WaitScreen.route) {
                             myComposable.Wrapper {
                                 myComposable.WaitScreen()
                             }
                         }
-                        composable(Routes.Hero.route+"/{id}"){ navBackStack ->
+                        composable(Routes.Hero.route + "/{id}") { navBackStack ->
                             val id = navBackStack.arguments?.getString("id").toString()
                             myComposable.Wrapper {
                                 myComposable.HeroScreen(id)

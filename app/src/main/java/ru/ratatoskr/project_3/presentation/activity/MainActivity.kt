39626c7458781a983b70537b5751b663ac967b46
Anton Guryev <ratatoskr2019@gmail.com>
2022-03-21 18:45:57 +0300

    Video doublicats removed, Model Adapted

--- app/src/main/java/ru/ratatoskr/project_3/presentation/activity/MainActivity.kt
+++ app/src/main/java/ru/ratatoskr/project_3/presentation/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.ratatoskr.project_3.presentation.activity
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -10,46 +11,70 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.LiveData
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.Timeline
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Log
+import com.google.android.exoplayer2.util.Util
 import dagger.hilt.android.AndroidEntryPoint
+import io.ktor.client.*
+import ru.ratatoskr.project_3.data.storage.RoomAppDatabase
 import ru.ratatoskr.project_3.domain.helpers.Screens
+import ru.ratatoskr.project_3.domain.helpers.states.VideoState
 import ru.ratatoskr.project_3.presentation.screens.*
 import ru.ratatoskr.project_3.presentation.viewmodels.*
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+
 
     @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             MainScreen()
+        }
+
+    }
+
+}
+
+fun stopPlayer(state: State<VideoState?>){
+    when (val state = state.value) {
+        is VideoState.PlayerState -> {
+            state.player.stop()
         }
     }
 }
 
 @ExperimentalFoundationApi
 @Composable
+
 fun MainScreen(
 ) {
 
     val navController = rememberNavController()
-
-
-
-
+    val videoViewModel = hiltViewModel<VideoViewModel>()
+    val videoViewState = videoViewModel.videoState.observeAsState()
     Scaffold(
         bottomBar = {
             BottomNavigation(
@@ -85,6 +110,7 @@ fun MainScreen(
             modifier = Modifier.padding(it)
         ) {
             composable(Screens.Home.route) {
+                stopPlayer(videoViewState)
                 val heroesListviewModel = hiltViewModel<HeroesListViewModel>()
                 HeroesListScreen(
                     viewModel = heroesListviewModel,
@@ -92,33 +118,38 @@ fun MainScreen(
                 )
             }
             composable(Screens.Hero.route + "/{id}") { navBackStack ->
+                stopPlayer(videoViewState)
                 val id = navBackStack.arguments?.getString("id").toString()
                 val heroViewModel = hiltViewModel<HeroViewModel>()
                 HeroScreen(
                     id,
                     heroViewModel,
-                    navController)
+                    navController
+                )
             }
             composable(Screens.Attr.route + "/{attr}") { navBackStack ->
+                stopPlayer(videoViewState)
                 val attr = navBackStack.arguments?.getString("attr")
                 val heroesListviewModel = hiltViewModel<HeroesListViewModel>()
                 HeroesByAttributeScreen(attr!!, heroesListviewModel, navController)
             }
             composable(Screens.Favorites.route) {
+                stopPlayer(videoViewState)
                 val heroesListviewModel = hiltViewModel<HeroesListViewModel>()
                 FavoritesScreen(heroesListviewModel, navController)
             }
             composable(Screens.Profile.route) {
+                stopPlayer(videoViewState)
                 val profileViewModel = hiltViewModel<ProfileViewModel>()
                 ProfileScreen(profileViewModel)
             }
             composable(Screens.Role.route + "/{role}") { navBackStack ->
+                stopPlayer(videoViewState)
                 val role = navBackStack.arguments?.getString("role")
                 val heroesListviewModel = hiltViewModel<HeroesListViewModel>()
                 HeroesByRoleScreen(role!!, heroesListviewModel, navController)
             }
             composable(Screens.Video.route) { navBackStack ->
-                val videoViewModel = hiltViewModel<VideoViewModel>()
                 VideoScreen(videoViewModel)
             }
         }

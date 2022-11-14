5e2b706ca996d011aa2805dda034d7ccf8b53de4
Ruslan425 <92014369+Ruslan425@users.noreply.github.com>
2022-11-14 21:37:52 +0300

    Change send form.

--- app/src/main/java/net/doheco/presentation/screens/profile/ProfileScreen.kt
+++ app/src/main/java/net/doheco/presentation/screens/profile/ProfileScreen.kt
@@ -8,8 +8,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
@@ -18,6 +21,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import net.doheco.R
@@ -189,15 +193,20 @@ fun ProfileScreen(
             onDismissRequest = {
                 openDialog.value = false
             },
-            backgroundColor = Color.Black,
+            backgroundColor = Color(0xFF131313),
+            shape = RoundedCornerShape(10.dp),
             text = {
                 Column() {
-                    Text(text = stringResource(id = R.string.offer), color = Color.White)
+                    Text(
+                        text = stringResource(id = R.string.offer),
+                        color = Color.White,
+                        modifier = Modifier.padding(bottom = 24.dp)
+                    )
                     OutlinedTextField(
                         value = titleText,
                         onValueChange = { titleText = it},
                         singleLine = true,
-                        label = { Text(text = stringResource(id = R.string.title))},
+                        label = { Text(text = stringResource(id = R.string.send_name))},
                         colors = TextFieldDefaults.outlinedTextFieldColors(
                             textColor = Color.White,
                             focusedBorderColor = Color.White,
@@ -223,22 +232,118 @@ fun ProfileScreen(
             },
             buttons = {
                 Column(
-                    modifier = Modifier.padding(all = 8.dp),
+                    modifier = Modifier.padding(horizontal = 24.dp),
                 ) {
                     Button(
                         modifier = Modifier.fillMaxWidth(),
-                        onClick = { openDialog.value = false }
+                        onClick = { openDialog.value = false },
+                        enabled = titleText.isNotEmpty() && messageText.isNotEmpty(),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color(0xFF00821d),
+                            disabledBackgroundColor = Color(0xFF6a36a3),
+                            contentColor = Color.White,
+                            disabledContentColor = Color.White
+                        )
                     ) {
                         Text(text = stringResource(id = R.string.send))
                     }
-                    Button(
-                        modifier = Modifier.fillMaxWidth(),
+                    OutlinedButton(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 26.dp),
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            contentColor = Color.White,
+                            backgroundColor = Color(0xFF131313)
+                        ),
                         onClick = { openDialog.value = false }
                     ) {
                         Text(text = stringResource(id = R.string.close))
                     }
+
                 }
             }
         )
     }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun Prew() {
+    val openDialog = remember { mutableStateOf(false) }
+    var titleText by remember { mutableStateOf("") }
+    var messageText by remember { mutableStateOf("") }
+    AlertDialog(
+        onDismissRequest = {
+            openDialog.value = false
+        },
+        backgroundColor = Color(0xFF131313),
+        shape = RoundedCornerShape(10.dp),
+        text = {
+            Column() {
+                Text(
+                    text = stringResource(id = R.string.offer),
+                    color = Color.White,
+                    modifier = Modifier.padding(bottom = 24.dp)
+                )
+                OutlinedTextField(
+                    value = titleText,
+                    onValueChange = { titleText = it},
+                    singleLine = true,
+                    label = { Text(text = stringResource(id = R.string.send_name))},
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        textColor = Color.White,
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        focusedLabelColor = Color.White,
+                        unfocusedLabelColor = Color.White
+                    )
+                )
+                OutlinedTextField(
+                    value = messageText,
+                    onValueChange = { messageText = it},
+                    label = { Text(text = stringResource(id = R.string.message)) },
+                    modifier = Modifier.height(150.dp),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        textColor = Color.White,
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        focusedLabelColor = Color.White,
+                        unfocusedLabelColor = Color.White
+                    )
+                )
+            }
+        },
+        buttons = {
+            Column(
+                modifier = Modifier.padding(horizontal = 24.dp),
+            ) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { openDialog.value = false },
+                    enabled = titleText.isNotEmpty() && messageText.isNotEmpty(),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(0xFF00821d),
+                        disabledBackgroundColor = Color(0xFF6a36a3),
+                        contentColor = Color.White,
+                        disabledContentColor = Color.White
+                    )
+                ) {
+                    Text(text = stringResource(id = R.string.send))
+                }
+                OutlinedButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 26.dp),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        contentColor = Color.White,
+                        backgroundColor = Color(0xFF131313)
+                    ),
+                    onClick = { openDialog.value = false }
+                ) {
+                    Text(text = stringResource(id = R.string.close))
+                }
+            }
+        }
+    )
 }

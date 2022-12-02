da71b431374300c64d1a42387fda2a52c7094b91
Anton Guryev <ratatoskr2019@gmail.com>
2022-12-02 14:40:47 +0300

    Exit fixed

--- app/src/main/java/net/doheco/presentation/screens/profile/views/DefinedBySteamProfileView.kt
+++ app/src/main/java/net/doheco/presentation/screens/profile/views/DefinedBySteamProfileView.kt
@@ -70,7 +70,16 @@ fun DefinedBySteamProfileView(
         ) {
 
             stickyHeader {
-                ProfileHeaderView(navController, state, viewModel, player_tier,onReloadClick,dialogState,true)
+                ProfileHeaderView(
+                    navController,
+                    state,
+                    viewModel,
+                    player_tier,
+                    onReloadClick,
+                    dialogState,
+                    true,
+                    onSteamExit
+                )
             }
             /*
             item {
@@ -160,7 +169,10 @@ fun DefinedBySteamProfileView(
             if (state.matchesList != null) {
                 items(state.matchesList!!) { item ->
                     var bgColor = Color(0xFF7E0000)
-                    if (item.radiantWin!! && item.playerSlot!! < 127) {
+                    if (item.radiantWin!! && item.playerSlot!! <= 127) {
+                        bgColor = Color(0xFF007E00)
+                    }
+                    if (!item.radiantWin && item.playerSlot!! > 127) {
                         bgColor = Color(0xFF007E00)
                     }
                     Row(
@@ -178,7 +190,6 @@ fun DefinedBySteamProfileView(
                         ) {
 
 
-
                             Image(
                                 contentDescription = item.matchId.toString(),
                                 painter = loadPicture(
@@ -188,7 +199,7 @@ fun DefinedBySteamProfileView(
                                 modifier = Modifier
                                     .width(60.dp)
                                     .height(35.dp)
-                                    .padding(top = 5.dp, bottom = 5.dp, end=15.dp)
+                                    .padding(top = 5.dp, bottom = 5.dp, end = 15.dp)
 
                             )
 
